10dfdabc6f002cedcf6f0f99f2e800f015765bb5
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-11-03 19:27:00 +0000

    Mon, Nov 3, 2025, 11:27 AM -08:00

--- faces/faces.py
+++ faces/faces.py
@@ -1,7 +1,10 @@
-def convert():
-    x=input()
+def convert(x):
     step_1 = x.replace(":)","🙂")
     step_2 = step_1.replace(":(","🙁")
     print(step_2)
 
-convert()
+def main():
+    poop = input()
+    print(convert.poop)
+
+main()
