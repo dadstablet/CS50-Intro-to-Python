b1f0a16e8d0ec967bfbac63066c17a14274c6526
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-11-05 00:54:34 +0000

    Tue, Nov 4, 2025, 4:54 PM -08:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,6 +1,6 @@
 x = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 
-clean_x = x.trim().lower()
+clean_x = str(x).strip().lower()
 if clean_x == "42": print("Yes")
 elif clean_x == "forty-two": print("Yes")
 elif clean_x == "forty two": print("Yes")
