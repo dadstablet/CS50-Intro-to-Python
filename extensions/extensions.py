a54aaab9f9abc49997f7242d45d737d3c6a807a2
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-11-12 23:50:15 +0000

    Wed, Nov 12, 2025, 3:50 PM -08:00

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,5 +1,9 @@
 x = input("File name: ").lower().strip()
 
 if x.endswith(".gif"): print("image/gif")
-elif
+elif x.endswith(".jpeg", ".jpg"): print("image/jpeg")
+elif x.endswith(".png"): print("image/png")
+elif x.endswith(".pdf"): print("application/pdf")
+elif x.endswith(".txt"): print("text/plain")
+elif x.endswith(".zip"): print("application/zip")
 else: print("application/octet-stream")
