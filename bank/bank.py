bfa5f6348a3e4761561fb9de9178ee68ad65100c
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-11-12 23:37:12 +0000

    Wed, Nov 12, 2025, 3:37 PM -08:00

--- bank/bank.py
+++ bank/bank.py
@@ -2,6 +2,7 @@ x = input("Greeting: ")
 
 clean_x = x.strip().lower()
 
-if clean_x.startswith("hello") == "True": print("$0")
-# elif clean_x.startswith("hello") == FALSE AND clean_x.startswith("h") == TRUE: print ("$20")
-else: print("$100")
+if clean_x.startswith("hello"): print("$0")
+else:
+    if clean_x.startswith("h"): print ("$20")
+    else: print("$100")
