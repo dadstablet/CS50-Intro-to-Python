3eab62b7ad2a1e6a18b0c27636056c22fa66ddbe
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-11-13 00:13:00 +0000

    Wed, Nov 12, 2025, 4:13 PM -08:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -3,3 +3,9 @@ user_array = user_input.split(" ")
 # print(user_array)
 x = int(user_array[0])
 z = int(user_array[2])
+y = str(user_array[1])
+
+if y == "+": result = float(x + z, 1)
+else: result = "bruh"
+
+print(result)
