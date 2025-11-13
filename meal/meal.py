1abbc8118488e4d77a65c3576bdc1e2cd8ecc4b4
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-11-13 17:46:39 +0000

    Thu, Nov 13, 2025, 9:46 AM -08:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,9 +1,11 @@
 def main():
     x = input("What time is it? ")
     return(x)
+    convert(x)
 
-# def convert(time):
-#     ...
+def convert(time):
+    return(x)
+    print(x)
 
 
 if __name__ == "__main__":
