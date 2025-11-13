85dd6f202dbe82e9ffbd85a9a22779e573e82649
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-11-13 19:47:55 +0000

    Thu, Nov 13, 2025, 11:47 AM -08:00

--- meal/meal.py
+++ meal/meal.py
@@ -1,10 +1,9 @@
 def main():
-    x = input("What time is it? ")
-    convert(x)
-    print(x)
-    # if 7 <= x <= 8: print("breakfast time")
-    # elif 12 <= x <= 13: print("lunch time")
-    # elif 18 <= x <= 19: print("dinner time")
+    user = input("What time is it? ")
+    x = convert(user)
+    if 7 <= x <= 8: print("breakfast time")
+    elif 12 <= x <= 13: print("lunch time")
+    elif 18 <= x <= 19: print("dinner time")
 
 def convert(time):
     hours, minutes = time.split(":")
