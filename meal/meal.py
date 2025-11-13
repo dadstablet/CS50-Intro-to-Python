4f8de9bcb5b0e34f41e4b1cd70f6fb4a9e8b3b62
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-11-13 19:39:25 +0000

    Thu, Nov 13, 2025, 11:39 AM -08:00

--- meal/meal.py
+++ meal/meal.py
@@ -4,8 +4,10 @@ def main():
 
 def convert(time):
     hours, minutes = time.split(":")
-    clock = int(hours) + (int(minutes)/60)
-    print(clock)
+    sum_hr_min = float(int(hours) + (int(minutes)/60))
+    if 7 <= sum_hr_min <= 8: print("breakfast time")
+    elif 12 <= sum_hr_min <= 13: print("lunch time")
+    elif 18 <= sum_hr_min <= 19: print("dinner time")
 
 if __name__ == "__main__":
     main()
