ff71945310787db85bcf8f61057d35308e6cf137
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-11-13 00:14:38 +0000

    Wed, Nov 12, 2025, 4:14 PM -08:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -5,7 +5,8 @@ x = int(user_array[0])
 z = int(user_array[2])
 y = str(user_array[1])
 
-if y == "+": result = float(x + z, 1)
+if y == "+": result = round(x + z, 1)
+elif y == "-": result = round(x - z, 1)
 else: result = "bruh"
 
 print(result)
