ae8a54f8a47fd59c0f8499b378238ce297d14a01
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-11-14 18:26:39 +0000

    Fri, Nov 14, 2025, 10:26 AM -08:00

--- camel/camel.py
+++ camel/camel.py
@@ -1,10 +1,8 @@
 x = str(input("camelCase: "))
 
-def snake(user_input):
-    for char in user_input:
-        if char.islower() == False:
-            lower_char = char.lower()
-            snake_char = "_"+lower_char
-            char = snake_char
-
-snake(x)
+for char in x:
+    if char.islower() == False:
+        lower_char = char.lower()
+        snake_char = "_"+lower_char
+        char = snake_char
+    print(char, end="")
