6a83361e4b36c37c49588ef7f713c2d0052f8e38
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-11-14 21:29:14 +0000

    Fri, Nov 14, 2025, 1:29 PM -08:00

--- coke/coke.py
+++ coke/coke.py
@@ -4,7 +4,8 @@ amount = 50
 while True:
     amount_prompt = print("Amount Due: ",amount)
     user_prompt = int(input("Insert Coin: "))
-    
+    z = user_prompt
+    amount = amount - z
     # if user_prompt != 50 or user_prompt != 25 or user_prompt != 10 or user_prompt != 5:
     #     break
 
