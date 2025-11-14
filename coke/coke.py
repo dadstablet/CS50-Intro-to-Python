69309175e35802515cc719bdc06ca5ddb664aa79
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-11-14 21:28:16 +0000

    Fri, Nov 14, 2025, 1:28 PM -08:00

--- coke/coke.py
+++ coke/coke.py
@@ -1,11 +1,10 @@
 amount = 50
-amount_prompt = print("Amount Due: ",amount)
-user_prompt = int(input("Insert Coin: "))
 
-if user_prompt == 50 or user_prompt == 25 or user_prompt == 10 or user_prompt == 5:
-    amount = user_prompt
-else:
-    amount_prompt
-# while amount >= 0:
-#     amount = amount - user_prompt
-#     user_prompt
+
+while True:
+    amount_prompt = print("Amount Due: ",amount)
+    user_prompt = int(input("Insert Coin: "))
+    
+    # if user_prompt != 50 or user_prompt != 25 or user_prompt != 10 or user_prompt != 5:
+    #     break
+
