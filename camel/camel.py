7cd82557faffd842a6632b75dd9a419f23f18556
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-11-14 18:13:56 +0000

    Fri, Nov 14, 2025, 10:13 AM -08:00

--- camel/camel.py
+++ camel/camel.py
@@ -4,4 +4,5 @@ for char in x:
     if char.islower() == False:
         lower_char = char.lower()
         snake_char = "_"+lower_char
-        print(snake_char, end="")
+        return(snake_char)
+    
