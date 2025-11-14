7ba7ade1026b88447c559cebd19281cb99a7c5b0
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-11-14 21:19:12 +0000

    Fri, Nov 14, 2025, 1:19 PM -08:00

--- coke/coke.py
+++ coke/coke.py
@@ -4,6 +4,8 @@ user_prompt = int(input("Insert Coin: "))
 
 if user_prompt == 50 OR user_prompt == 25 OR user_prompt == 10 OR user_prompt == 5:
     print(user_prompt)
+else:
+    amount
 # while amount >= 0:
 #     amount = amount - user_prompt
 #     user_prompt
