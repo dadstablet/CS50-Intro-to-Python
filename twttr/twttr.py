aeea66f36482ca20bc8de2c7b26b91b6196961e0
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-11-19 23:59:09 +0000

    Wed, Nov 19, 2025, 3:59 PM -08:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -4,6 +4,8 @@ def main():
     vowels = "AEOUIaeoui"
 
     x_list = [char for char in x if char not in vowels]
-    print(str(x_list))
+
+    for char in x_list:
+        print(char, end="")
 
 main()
