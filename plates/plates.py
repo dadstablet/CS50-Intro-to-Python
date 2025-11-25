f0534f54318021b28dd2d441270394a969cc9ffc
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-11-25 00:06:56 +0000

    Mon, Nov 24, 2025, 4:06 PM -08:00

--- plates/plates.py
+++ plates/plates.py
@@ -5,11 +5,40 @@ def main():
     else:
         print("Invalid")
 
+def is_first_digit_0(x):
+    for char in x:
+        if char.isdigit():
+            if char == '0':
+                return True
+            else:
+                return False
+    return False
+
+def num_between(x):
+    num_string = False
+    for char in x:
+        if char.isdigit():
+            num_string = True
+        elif char.isalpha():
+            if num_string == True:
+                return False
+    else:
+        return True
 
 def is_valid(s):
-    if 2 >= len(s) >= 6:
-        True
+    if s[:1].isnumeric():
+        return False
+    elif len(s) > 6:
+        return False
+    elif len(s) < 2:
+        return False
+    elif num_between(s) == False:
+        return False
+    elif is_first_digit_0(s):
+        return False
+    elif s.isalnum() == False:
+        return False
     else:
-        False
+        return True
 
 main()
