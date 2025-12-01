93b9358c129a0843062f5ef8658d9564e810a621
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-01 21:46:25 +0000

    Mon, Dec 1, 2025, 1:46 PM -08:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -22,14 +22,11 @@ def main():
         day_corrected = int(x_list[0])
         print(f"{year_corrected}-{month_corrected:02}-{day_corrected:02}")
     except IndexError:
-        # print("indexerror")
         try:
             x_list = x.split(" ")
             year_corrected = int(x_list[2])
             month_corrected = months[x_list[0]]
             day_corrected = int(x_list[1].strip(","))
-            # date_corrected = x_list[2]+'-'+str(months[x_list[0]])+'-'+x_list[1]
-            # print(date_corrected)
             print(f"{year_corrected}-{month_corrected:02}-{day_corrected:02}")
         except IndexError:
             print("IndexError")
