09237553f10276440c5efc0bd47456eadd7faf5c
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-01 18:11:51 +0000

    Mon, Dec 1, 2025, 10:11 AM -08:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -2,9 +2,9 @@ def main():
     while True:
         try:
             x = input("input: ")
+            store_x = [x]
         except EOFError:
             break
-        store_x = list(x)
     print(store_x)
 
 main()
