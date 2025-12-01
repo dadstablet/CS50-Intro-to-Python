9362173abbfdf44d801dcad53f4733d0f7caec15
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-01 19:40:09 +0000

    Mon, Dec 1, 2025, 11:40 AM -08:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -17,8 +17,9 @@ def main():
     x = input("Month: ")
     try:
         x_list = x.split("/")
-        print(x_list)
-    except SyntaxError:
-        print("SyntaxError")
+        date_corrected = str(x_list[2]+'-'+x_list[1]+'-'+x_list[0])
+        print(date_corrected)
+    except IndexError:
+        print("IndexError")
 
 main()
