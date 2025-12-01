649c3c07568846587b1a9dbf0f8a83b40c1e32e4
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-01 22:39:34 +0000

    Mon, Dec 1, 2025, 2:39 PM -08:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -30,11 +30,6 @@ def main():
             main()
         except KeyError:
             main()
-    # if month_corrected > 12:
-    #     main()
-    # elif day_corrected > 31:
-    #     main()
-    # else:
-    print(f"{year_corrected}-{month_corrected:02}-{day_corrected:02}")
+        print(f"{year_corrected}-{month_corrected:02}-{day_corrected:02}")
 
 main()
