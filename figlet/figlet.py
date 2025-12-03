b42437bcc6f7404b8dfb65a7fa6262d316b4d78e
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-03 19:07:26 +0000

    Wed, Dec 3, 2025, 11:07 AM -08:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -8,12 +8,12 @@ font_list = figlet.getFonts()
 if len(sys.argv) == 1:
     user_font = random.choice(font_list)
 elif len(sys.argv) == 3:
-    if sys.argv[1] != '-f' or '-font':
+    if sys.argv[1] != '-f':
         sys.exit("Invalid usage")
     else:
         user_font = sys.argv[2]
-# else:
-#     sys.exit("Invalid usage")
+else:
+    sys.exit("Invalid usage")
 
 x = input("Input: ")
 f = pyfiglet.figlet_format(x, font=user_font)
