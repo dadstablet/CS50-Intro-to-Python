488b3b1e4d27aea858bbbb91a76d1ef0649155cf
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-03 18:58:58 +0000

    Wed, Dec 3, 2025, 10:58 AM -08:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -5,11 +5,11 @@ import random
 figlet = pyfiglet.Figlet()
 font_list = figlet.getFonts()
 
-if len(sys.argv) == 3:
-    # f_input = sys.argv[1]
-    user_font = sys.argv[2]
-elif len(sys.argv) == 1:
+if len(sys.argv) == 1:
     user_font = random.choice(font_list)
+elif len(sys.argv) == 3:
+    user_font = sys.argv[2]
+    if 
 
 x = input("Input: ")
 f = pyfiglet.figlet_format(x, font=user_font)
