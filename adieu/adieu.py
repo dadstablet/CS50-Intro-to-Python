9f1ff7edb815b734eb6752bf9fa52974063e9307
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-03 22:01:58 +0000

    Wed, Dec 3, 2025, 2:01 PM -08:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -1,13 +1,14 @@
 import inflect
 p = inflect.engine()
 
-name_list = []
+def create_list():
+    name_list = []
+    while True:
+        try:
+            x = input("Name: ")
+            name_list.append(x)
+        except EOFError:
+            break
 
-while True:
-    try:
-        x = input("Name: ")
-        name_list.append(x)
-    except EOFError:
-        break
 output_list = p.join(name_list)
 output = print(f"Adieu, adieu, to {output_list}")
