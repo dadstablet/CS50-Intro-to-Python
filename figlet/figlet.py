4d59108aaa9384524e2991aafd640cee7914b8fc
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-03 18:23:51 +0000

    Wed, Dec 3, 2025, 10:23 AM -08:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -1,10 +1,12 @@
 import pyfiglet
 import sys
 
-f_input = sys.argv[1]
-font_name = sys.argv[2]
+if len(sys.argv) == 2:
+    f_input = sys.argv[1]
+    font_name = sys.argv[2]
+elif len(sys.argv) == 0:
 
 x = input("Input: ")
-
 f = pyfiglet.figlet_format(x, font=font_name)
 print(f)
+
