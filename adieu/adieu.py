67a9f2fd468061a4d9e51318b529dd047d864892
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-03 20:44:20 +0000

    Wed, Dec 3, 2025, 12:44 PM -08:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -4,5 +4,7 @@ while True:
     try:
         x = input("Name: ")
         name_list = name_list.append(x)
-    except 
+    except EOFError:
+        break
+
 output = print(f"Adieu, adieu, to {x}")
