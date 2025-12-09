862d06e61b34bc1e87269c9143d7cf137224226a
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-09 00:36:34 +0000

    Mon, Dec 8, 2025, 4:36 PM -08:00

--- game/game.py
+++ game/game.py
@@ -3,13 +3,17 @@ import random
 def main():
     try:
         x = int(input("Level: "))
+        return x
     except:
         main()
-    answer = random.randint(1, x)
-    if x == answer:
-        print("Just right!")
-    elif x < answer:
-        print("Too small!")
-    elif x > answer:
-        print("Too large!")
+
+answer = random.randint(1, x)
+if x == answer:
+    print("Just right!")
+elif x < answer:
+    print("Too small!")
+elif x > answer:
+    print("Too large!")
+
 main()
+print(answer)
