2c73ddae3f01efb1350849dcf2ae313c253ed324
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-10 21:10:25 +0000

    Wed, Dec 10, 2025, 1:10 PM -08:00

--- game/game.py
+++ game/game.py
@@ -1,23 +1,35 @@
 import random
 
+def set_level():
+    while True:
+        try:
+            level = int(input("Level: "))
+            if level > 0:
+                return level
+        except ValueError:
+            pass
+
 def main():
-    try:
-        level = int(input("Level: "))
-        x = int(input("Guess: "))
-        check(level, x)
-    except SyntaxError:
-        pass
+    level = set_level()
+    while True:
+        try:
+            x = int(input("Guess: "))
+            if x > 0:
+                help = check(level, x)
+            if help == "stop":
+                break
+        except ValueError:
+            pass
 
 def check(a, b):
     answer = random.randint(1, a)
     if b == answer:
         print("Just right!")
+        return "stop"
     elif b < answer:
         print("Too small!")
-        main()
     elif b > answer:
         print("Too large!")
-        main()
 
 main()
 
