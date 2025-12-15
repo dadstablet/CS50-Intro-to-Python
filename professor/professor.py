9420ff985819814daf9cd22a0e3d531759efd995
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-15 19:07:28 +0000

    Mon, Dec 15, 2025, 11:07 AM -08:00

--- professor/professor.py
+++ professor/professor.py
@@ -3,8 +3,7 @@ import random
 
 def main():
     user_level = get_level
-    generate_integer(user_level)
-    print
+    print(generate_integer(user_level))
 
 
 def get_level():
@@ -20,16 +19,19 @@ def get_level():
             pass
 
 def generate_integer(level):
-    if level = 1:
-        X = random.randint(1, 9)
-        Y = random.randint(1, 9)
-    elif level = 2:
-        X = random.randint(10, 99)
-        Y = random.randint(10, 99)
-    elif level = 3:
-        X = random.randint(100, 999)
-        Y = random.randint(100, 999)
-    return X and Y
+    try:
+        if level == 1:
+            X = random.randint(1, 9)
+            Y = random.randint(1, 9)
+        elif level == 2:
+            X = random.randint(10, 99)
+            Y = random.randint(10, 99)
+        elif level == 3:
+            X = random.randint(100, 999)
+            Y = random.randint(100, 999)
+        return X and Y
+    except ValueError:
+        return 'u trash'
 
 
 if __name__ == "__main__":
