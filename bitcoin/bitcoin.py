4c345d39f3d631c6f90dafcdf4a3e7e3bc422e5e
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-15 23:19:20 +0000

    Mon, Dec 15, 2025, 3:19 PM -08:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -4,9 +4,16 @@ import sys
 try:
     n = float(sys.argv[1])
 except ValueError:
-    sys.exit("u trash")
+    sys.exit("Command-line argument is not a number")
+except IndexError:
+    sys.exit("Missing command-line argument")
 
 try:
-    r = requests.get()
+    r = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=aa6e170cd0321a44cc4ce8d7c81ad3d6388399c67a829f3cf4e678ea97675e37")
+    content = r.json()
+    price = float(content["data"]["priceUsd"])
+    user_price = n * price
+    print(f"${user_price:,.4f}")
 except requests.RequestException:
+    pass
 
