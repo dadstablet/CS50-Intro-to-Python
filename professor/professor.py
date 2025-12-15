5ecdce7da47c5ef655f574079ea59ecb0f948d5b
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-15 20:39:57 +0000

    Mon, Dec 15, 2025, 12:39 PM -08:00

--- professor/professor.py
+++ professor/professor.py
@@ -17,15 +17,15 @@ def get_level():
         else:
             pass
 
+
 def generate_integer(level):
     if level == 1:
-        return random.randint(1, 9)
-        # Y = random.randint(1, 9)
+        return [random.randint(1, 9), random.randint(1, 9)]
     elif level == 2:
-        return random.randint(10, 99)
+        return [random.randint(10, 99), random.randint(10, 99)]
         # Y = random.randint(10, 99)
     elif level == 3:
-        return random.randint(100, 999)
+        return [random.randint(100, 999), random.randint(10, 99)]
         # Y = random.randint(100, 999)
     else:
         raise ValueError
