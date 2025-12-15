867e8c0327d8710fc7fde9021c8b88cfd210d3ff
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-15 21:02:49 +0000

    Mon, Dec 15, 2025, 1:02 PM -08:00

--- professor/professor.py
+++ professor/professor.py
@@ -9,16 +9,16 @@ def main():
         X = generate_integer(user_level)[0]
         Y = generate_integer(user_level)[1]
         Q = input(f"{X} + {Y} = ")
-    #     if Q == (X + Y):
-    #         print("")
-    #         score += 1
-    #     else:
-    #         print("EEE")
-    #     _ += 1
-    # print(f"Score: {score}")
-        print(X+Y)
-        print(Q)
-        _ += 1
+        if Q == (X + Y):
+            print("")
+            score += 1
+        # else:
+        #     print("EEE")
+        # _ += 1
+    print(f"Score: {score}")
+        # print(X+Y)
+        # print(Q)
+        # _ += 1
 
 def get_level():
     while True:
