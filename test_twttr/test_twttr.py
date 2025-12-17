8e46e2e57d8eddfc33e2bf0be000ab4bb149ea26
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-17 23:43:20 +0000

    Wed, Dec 17, 2025, 3:43 PM -08:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -9,6 +9,6 @@ def test_HELLO():
     x = 'HELLO'
     assert shorten(x) == 'HLL'
 
-# def test_int():
-#     x = random.randint(-999,999)
-#     assert shorten(x) == x
+def test_int():
+    x = random.randint(-999,999)
+    assert shorten(x) == x
