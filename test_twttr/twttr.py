85b8dcfab020b21d6a18bbed4b98afa7ab694b4c
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-17 23:34:24 +0000

    Wed, Dec 17, 2025, 3:34 PM -08:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -3,7 +3,7 @@ def main():
     print(shorten(x))
 
 def shorten(word):
-    vowels = "AOUIEaeoui" 
+    vowels = "AOUIEaeoui"
     try:
         word = [char for char in word if char not in vowels]
         short_word = []
@@ -11,7 +11,7 @@ def shorten(word):
             short_word.append(char)
         return ''.join(short_word)
     except TypeError:
-        return word
+        return "Error"
 
 if __name__ == "__main__":
     main()
