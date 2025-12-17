9e6d900a2c68435b37f63d53a0fccf64d2502b5d
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-17 23:18:56 +0000

    Wed, Dec 17, 2025, 3:18 PM -08:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,7 +1,9 @@
 from twttr import shorten
 
-def test_hello:
-    x = input("Type hello :")
-    shorten(x)
+def test_hello():
+    x = input("Type hello : ")
+    char for char in x if 
+    assert shorten(x) == "hll"
+
 
 test_hello()
