bca98f7ea73e60646fa604595381612042b9306c
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-18 17:53:36 +0000

    Thu, Dec 18, 2025, 9:53 AM -08:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -3,3 +3,15 @@ from plates import is_valid
 def test_True():
     assert is_valid("AAA222") == True
 
+def test_short():
+    assert is_valid("H") == False
+
+def test_nums():
+    assert is_valid("AAA022") == False
+    assert is_valid("BB222A") == False
+    assert is_valid("23AAA8") == False
+
+def test_other():
+    assert is_valid("zzz.") == False
+    assert is_valid("AA A2") == False
+    assert is_valid("A AA.2") == False
