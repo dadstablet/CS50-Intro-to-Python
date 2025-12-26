2f2b60bafaed73aeedeb748934001415208ee324
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-26 21:35:58 +0000

    Fri, Dec 26, 2025, 1:35 PM -08:00

--- lines/lines.py
+++ lines/lines.py
@@ -1,19 +1,19 @@
 import sys
 
 def main():
-    x = sys.argv[1:]
+    x = sys.argv[1]
     check(x)
 
 
 def check(f):
     try:
-        
-        if f :#contains .py and leads to existing file
-            pass
-        else:
-            raise FileNotFoundError
-    except FileNotFoundError:
-        sys.exit("File does not exist")
+        with open(f, "r") as file:
+            if f :#contains .py and leads to existing file
+                pass
+            else:
+                raise FileNotFoundError
+        except FileNotFoundError:
+            sys.exit("File does not exist")
 
 if __name__ == "__main__":
     main()
