1f4c1c8b75e5f655fc3211c99091ba55e3f6b230
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-30 17:24:11 +0000

    Tue, Dec 30, 2026, 9:24 AM -08:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,7 +1,20 @@
 import csv
 import tabulate
+import sys
 
 def main():
+    usr_input = sys.argv[:]
+    if len(usr_input) > 2:
+        sys.exit("Too many command-line argumets")
+    elif len(usr_input) == 1:
+        sys.exit("Too few command-line arguments")
+    else:
+        try:
+            read_file(usr_input)
+        except 
+
+
+def read_file(x):
     with open("sicilian.csv", "r") as sicilian, open("regular.csv", "r") as regular:
         sicilian = csv.DictReader(sicilian)
         regular = csv.DictReader(regular)
