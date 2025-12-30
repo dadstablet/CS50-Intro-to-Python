af10be25a379f45c1ecbd84e1131b0917d20122f
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2025-12-30 17:13:57 +0000

    Tue, Dec 30, 2026, 9:13 AM -08:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,5 +1,13 @@
 import csv
 import tabulate
 
-def main()
-    with
+def main():
+    with open("sicilian.csv", "r") as sicilian, open("regular.csv", "r") as regular:
+        sicilian = csv.DictReader(sicilian)
+        regular = csv.DictReader(regular)
+
+        for row in sicilian:
+            print(row)
+
+if __name__ == "__main__":
+    main()
