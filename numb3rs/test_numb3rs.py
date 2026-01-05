68624cd126aff1b61f94df1c4b08e2740e42bba3
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-01-05 21:52:59 +0000

    Mon, Jan 5, 2026, 1:52 PM -08:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -2,14 +2,13 @@ from numb3rs import validate
 import pytest
 
 def test_lead0():
-    assert validate("001.22.22.22") == "Invalid"
-    assert validate("0.00.00.0") == "Invalid"
+    assert validate("001.22.22.22") == "False"
+    assert validate("0.00.00.0") == "False"
 
 def test_chars():
-    with pytest.raises(AttributeError):
-        # validate("dog.cat")
-        validate("2.2.2.cow")
+    assert validate("dog.cat") == "False"
+    assert validate("2.2.2.cow") == "False"
 
 def test_greaterthan255():
-    assert validate("260.2.2.2") == "Invalid"
-    assert validate("22.9.2.260") == "Invalid"
+    assert validate("260.2.2.2") == "False"
+    assert validate("22.9.2.260") == "False"
