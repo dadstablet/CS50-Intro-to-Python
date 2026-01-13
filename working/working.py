e0b601d033707eeec88e366730aadd8ab0f9ff37
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-01-13 19:12:51 +0000

    Tue, Jan 13, 2026, 11:12 AM -08:00

--- working/working.py
+++ working/working.py
@@ -8,7 +8,7 @@ def main():
 
 def convert(s):
     # re.search(r"9:00 AM to 5:00 PM",s)
-    time = re.search(r"([1-2]?[0-9]:[0-5][0-9])",s)
+    time = re.search(r"([1-2]?[0-9]:[0-5][0-9]) ()",s)
     return time.group(1)
 
 ...
