1e562bc196feed371fdd1e4549a8f19d285aab92
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-01-13 21:55:06 +0000

    Tue, Jan 13, 2026, 1:55 PM -08:00

--- working/working.py
+++ working/working.py
@@ -12,7 +12,7 @@ def main():
 def convert(s):
     # re.search(r"9:00 AM to 5:00 PM",s)
     try:
-        time = re.search(r"^([1-2]?[0-9]){1}:?([0-5][0-9])? (am|pm|AM|PM) to ([1-2]?[0-9]){1}:?([0-5][0-9])? (am|pm|AM|PM)$",s)
+        time = re.search(r"^([1]?[0-9]){1}:?([0-5][0-9])? (am|pm|AM|PM) to ([1]?[0-9]){1}:?([0-5][0-9])? (am|pm|AM|PM)$",s)
         start_hour, start_min, start_day, end_hour, end_min, end_day = time.groups()
         # return start_hour, start_min, start_day, end_hour, end_min, end_day
         if start_day.lower() == "am":
