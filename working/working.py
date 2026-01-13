62eb17a8e0c1061af965f29dc2cb32551e21131e
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-01-13 21:06:55 +0000

    Tue, Jan 13, 2026, 1:06 PM -08:00

--- working/working.py
+++ working/working.py
@@ -8,10 +8,12 @@ def main():
 
 def convert(s):
     # re.search(r"9:00 AM to 5:00 PM",s)
-    time = re.search(r"^((?:[1-2]?[0-9]){1}(?::[0-5][0-9])?) ([aApPmM]{2}) to ((?:[1-2]?[0-9]){1}(?::[0-5][0-9])?) ([aApPmM]{2})$",s)
-    start_time, start_day, end_time, end_day = time.groups()
-    return start_time, start_day, end_time, end_day
-...
+    try:
+        time = re.search(r"^((?:[1-2]?[0-9]){1}(?::[0-5][0-9])?) ([aApPmM]{2}) to ((?:[1-2]?[0-9]){1}(?::[0-5][0-9])?) ([aApPmM]{2})$",s)
+        start_time, start_day, end_time, end_day = time.groups()
+        return start_time, start_day, end_time, end_day
+    except AttributeError:
+        sys.exit("AttributeError")
 
 
 if __name__ == "__main__":
