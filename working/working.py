2084a4b5c745062cccfe6e5392c5d5126ea75f88
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-01-13 21:28:01 +0000

    Tue, Jan 13, 2026, 1:28 PM -08:00

--- working/working.py
+++ working/working.py
@@ -11,7 +11,7 @@ def convert(s):
     try:
         time = re.search(r"^([1-2]?[0-9]){1}:?([0-5][0-9])? (am|pm|AM|PM) to ([1-2]?[0-9]){1}:?([0-5][0-9])? (am|pm|AM|PM)$",s)
         start_hour, start_min, start_day, end_hour, end_min, end_day = time.groups()
-        return time.groups()
+        return start_hour, start_min, start_day, end_hour, end_min, end_day
     except AttributeError:
         sys.exit("AttributeError")
 
