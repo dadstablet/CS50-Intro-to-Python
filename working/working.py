1a003a1721c61d5a682afc1404919975b5dd23fb
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-01-20 16:46:58 +0000

    Tue, Jan 20, 2026, 8:46 AM -08:00

--- working/working.py
+++ working/working.py
@@ -12,21 +12,24 @@ def main():
 def convert(s):
     # re.search(r"9:00 AM to 5:00 PM",s)
     try:
-        time = re.search(r"^([1]?[0-9]){1}:?([0-5][0-9])? (am|pm|AM|PM) to ([1]?[0-9]){1}:?([0-5][0-9])? (am|pm|AM|PM)$",s)
-        start_hour, start_min, start_day, end_hour, end_min, end_day = time.groups()
-        if start_day.lower() == "am":
-            if int(start_hour) < 10:
-                start_hour = "0"+start_hour
-            elif 10 < int(start_hour) < 12:
-                start_hour = start_hour
-            elif int(start_hour) == 12:
-                start_hour = "00"
-        elif start_day.lower() == "pm":
-            if int(start_hour) == 12:
-                start_hour = "12"
-            elif int(start_hour) < 12:
-                start_hour = int(start_hour)
-                start_hour = start_hour + 12
+        s_list = s.split(" to ", 1)
+    try:
+        for time in s_list:
+            re.search(r"^([1]?[0-9]){1}:?([0-5][0-9])? (am|pm|AM|PM), time
+            start_hour, start_min, start_day = time.groups()
+            if start_day.lower() == "am":
+                if int(start_hour) < 10:
+                    start_hour = "0"+start_hour
+                elif 10 < int(start_hour) < 12:
+                    start_hour = start_hour
+                elif int(start_hour) == 12:
+                    start_hour = "00"
+            elif start_day.lower() == "pm":
+                if int(start_hour) == 12:
+                    start_hour = "12"
+                elif int(start_hour) < 12:
+                    start_hour = int(start_hour)
+                    start_hour = start_hour + 12
 
         return f"{start_hour}:{start_min} {start_day.upper()}"
         # return time.groups()
