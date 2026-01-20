58eb15c9dca81106ae9b983807d1ef2d33792f91
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-01-20 16:56:23 +0000

    Tue, Jan 20, 2026, 8:56 AM -08:00

--- working/working.py
+++ working/working.py
@@ -13,13 +13,13 @@ def convert(s):
     # re.search(r"9:00 AM to 5:00 PM",s)
     try:
         s_list = s.split(" to ", 1)
-        return s_list
+        # return s_list
     except AttributeError:
         sys.exit("AttributeError")
 
     for item in s_list:
-        try
-            time = re.search(r"^([1]?[0-9]){1}:?([0-5][0-9])? (am|pm|AM|PM), time)
+        try:
+            time = re.search(r"^([1]?[0-9]){1}:?([0-5][0-9])? (am|pm|AM|PM)", item)
             start_hour, start_min, start_day = time.groups()
             if start_day.lower() == "am":
                 if int(start_hour) < 10:
@@ -35,10 +35,10 @@ def convert(s):
                     start_hour = int(start_hour)
                     start_hour = start_hour + 12
 
-        return f"{start_hour}:{start_min} {start_day.upper()}"
-        # return time.groups()
-    except AttributeError:
-        sys.exit("AttributeError")
+            return f"{start_hour}:{start_min} {start_day.upper()}"
+            # return time.groups()
+        except AttributeError:
+            sys.exit("AttributeError")
 
 
 if __name__ == "__main__":
