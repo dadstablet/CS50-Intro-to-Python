ace5836df9a2044188ee17abe364a8593fd0bd43
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-01-26 23:39:50 +0000

    Mon, Jan 26, 2026, 3:39 PM -08:00

---
+++ um/um.py
@@ -0,0 +1,19 @@
+import re
+import sys
+
+
+def main():
+    print(count(input("Text: ")))
+
+
+def count(s):
+    um_count = re.search(r"um",s)
+    ums = 0
+    for um in um_count.groups():
+        ums += 1
+    return ums
+...
+
+
+if __name__ == "__main__":
+    main()
