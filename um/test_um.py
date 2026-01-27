4f8cf0c4435904b82449b2e200a6b978843e729c
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-01-27 00:18:55 +0000

    Mon, Jan 26, 2026, 4:18 PM -08:00

--- um/test_um.py
+++ um/test_um.py
@@ -2,5 +2,9 @@ from um import count
 import pytest
 
 def test_yumm():
-    assert count("yumm") == "0"
-    assert count("mum bum") == "0"
+    assert count("um") == 0
+    assert count("mum bum") == 0
+
+def test_case():
+    assert count("Um, UM!") == 2
+    assert count("UM, hi") == 1
