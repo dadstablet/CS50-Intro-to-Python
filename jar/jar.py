463d4a31deb549a5e4fa32783b46a17fcdf0da61
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-01-29 18:59:17 +0000

    Thu, Jan 29, 2026, 10:59 AM -08:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,20 +1,23 @@
 class Jar:
     def __init__(self, capacity=0):
-        if capacity > 12:
-            raise ValueError
-        elif capacity < 0:
-            raise ValueError
-        else:
-            self._capacity = capacity
+        self._capacity = capacity
 
     def __str__(self):
         return f"{self._capacity * '🍪'}"
 
     def deposit(self, n):
         self._capacity += n
+        if self._capacity > 12:
+            raise ValueError
+        elif n < 0:
+            raise ValueError
 
     def withdraw(self, n):
         self._capacity -= n
+        if self._capacity < 0:
+            raise ValueError
+        elif n < 0:
+            raise ValueError
 
     @property
     def capacity(self):
