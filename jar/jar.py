119af4020a269d10bd1ff233d926d28fbd3c5436
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-01-29 19:05:36 +0000

    Thu, Jan 29, 2026, 11:05 AM -08:00

--- jar/jar.py
+++ jar/jar.py
@@ -11,16 +11,12 @@ class Jar:
 
     def deposit(self, n):
         self._size += n
-        if self._capacity > 12:
-            raise ValueError
-        elif n < 0:
+        if n < 0:
             raise ValueError
 
     def withdraw(self, n):
         self._capacity -= n
-        if self._capacity < 0:
-            raise ValueError
-        elif n < 0:
+        if n < 0:
             raise ValueError
 
     @property
