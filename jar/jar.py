c90968b8cbc2cf30f7e583d14aaa10d651d48bf8
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-01-29 21:41:53 +0000

    Thu, Jan 29, 2026, 1:41 PM -08:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,6 +1,6 @@
 class Jar:
     def __init__(self, capacity=12):
-        self._capacity = 12
+        self._capacity = capacity
         self._size = 0
         if self._size > capacity:
             raise ValueError
