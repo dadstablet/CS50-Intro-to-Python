c798ec45b1ae48a36c78a8cc6724f69ba9c8f96e
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-03 20:54:34 +0000

    Tue, Feb 3, 2026, 12:54 PM -08:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -3,5 +3,5 @@ from fpdf import FPDF
 pdf = FPDF() #automatically Portrait, A4, millimeters
 pdf.add_page()
 pdf.set_font("helvetica", style="B", size=16)
-pdf.cell(105, 10, "CS50 Shirtificate")
+pdf.cell(105, 10, "CS50 Shirtificate", align='C')
 pdf.output("tuto1.pdf")
