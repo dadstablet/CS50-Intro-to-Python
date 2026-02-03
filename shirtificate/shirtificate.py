ccd31666fa7d46fe63e58c95659e32ae0e41cec1
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-03 21:59:22 +0000

    Tue, Feb 3, 2026, 1:59 PM -08:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -5,7 +5,7 @@ class PDF(FPDF):
     #     self.add_page()
     def header(self):
         pdf.set_font('Times', size=12)
-        self.cell(30, 10, 'CS50 Shirtificate', align='C')
+        self.cell(30, 10, 'hello', align='C')
 
     # def shirt(self):
     #     pdf.set_font('Times', size=12)
