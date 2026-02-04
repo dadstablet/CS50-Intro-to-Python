ca0f847809bcb0e7c3e2e62c4243a4f4a9723ec5
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-04 21:07:13 +0000

    Wed, Feb 4, 2026, 1:07 PM -08:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -12,7 +12,7 @@ class PDF(FPDF):
     def shirt_name(self, name):
         self.set_font('Times', size =12)
         self.image("/workspaces/153245272/shirtificate/shirtificate.png", x=(210-100)/2, y=30, w=100, h=0) #center = page_width - image_width /2
-        self.cell(30, 10, f'{name} took CS50', align='C')
+        self.cell(100, 10, f'{name} took CS50', align='C')
 
 pdf = PDF()
 pdf.add_page()
