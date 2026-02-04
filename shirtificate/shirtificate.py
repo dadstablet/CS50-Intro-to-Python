95c889e55148d1fe208751de7d8087530237e247
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-04 21:27:40 +0000

    Wed, Feb 4, 2026, 1:27 PM -08:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,11 +1,10 @@
 from fpdf import FPDF
 
 class PDF(FPDF):
-#     # def __init__(self):
-#     #     self.add_page()
-    def header(self):
+
+    def title_text(self):
         self.set_font('Times', size=12)
-        self.cell(80)
+        self.cell(80, 0)
         self.cell(30, 10, 'CS50 Shirtificate', align='C')
         self.ln(20)
 
