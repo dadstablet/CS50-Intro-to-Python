ee5b2fe161c43a1270c4a3d9c6a83d969aa3ed8d
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-04 19:16:21 +0000

    Wed, Feb 4, 2026, 11:16 AM -08:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -1,16 +1,22 @@
 from fpdf import FPDF
 
-class PDF(FPDF):
-    # def __init__(self):
-    #     self.add_page()
-    # def header(self):
-    #     self.set_font('Times', size=12)
-    #     self.cell(80)
-    #     self.cell(30, 10, 'CS50 Shirtificate', align='C')
+# class PDF(FPDF):
+#     # def __init__(self):
+#     #     self.add_page()
+#     # def header(self):
+#     #     self.set_font('Times', size=12)
+#     #     self.cell(80)
+#     #     self.cell(30, 10, 'CS50 Shirtificate', align='C')
 
-    def shirt(self):
-        self.image('/workspaces/153245272/shirtificate/shirtificate.png', x=20, y=60)
+#     def shirt(self):
+#         self.image('/workspaces/153245272/shirtificate/shirtificate.png', x=20, y=60)
 
-pdf = PDF()
+# pdf = PDF()
+# pdf.add_page()
+# pdf.output('test.pdf')
+
+
+pdf = FPDF()
 pdf.add_page()
-pdf.output('test.pdf')
+pdf.image("/workspaces/153245272/shirtificate/shirtificate.png.1", x=20, y=60)
+pdf.output("pdf-with-image.pdf")
