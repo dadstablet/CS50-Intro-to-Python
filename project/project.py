d68d65d8e4724a39d31823728dc4f1da568f5868
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-09 23:49:24 +0000

    Mon, Feb 9, 2026, 3:49 PM -08:00

--- project/project.py
+++ project/project.py
@@ -1,4 +1,7 @@
+import wikipediaapi
 
+wiki_wiki = wikipediaapi.Wikipedia(user_agent='MyProjectName (merlin@example.com)', language='en')
+page_py = wiki_wiki.page('Python_(programming_language)')
 
 def main()
     input
