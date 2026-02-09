96e33ed200f2f42a5b6eae8800b8b35212d1bf31
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-09 23:55:14 +0000

    Mon, Feb 9, 2026, 3:55 PM -08:00

--- project/project.py
+++ project/project.py
@@ -4,7 +4,7 @@ wiki_wiki = wikipediaapi.Wikipedia(user_agent='MusicProjectBot (nzakaria@hl.agen
 page_py = wiki_wiki.page('Python_(programming_language)')
 
 page_py = wiki_wiki.page('Python_(programming_language)')
-print("Page - Exists: %s" % page_py.exists())
+print(page_py.exists("List of Classical-era composers"))
 
 #Ask user of music era of interest. Return list of composers of that era
 def main():
