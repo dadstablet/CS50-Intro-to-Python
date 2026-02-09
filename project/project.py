c883cc6864c898994e029ebcefcfd2943ab17815
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-09 23:57:38 +0000

    Mon, Feb 9, 2026, 3:57 PM -08:00

--- project/project.py
+++ project/project.py
@@ -1,9 +1,9 @@
 import wikipediaapi
 
 wiki_wiki = wikipediaapi.Wikipedia(user_agent='MusicProjectBot (nzakaria@hl.agency)', language='en')
-page_py = wiki_wiki.page('Python_(programming_language)')
+page_py = wiki_wiki.page('List of Classical-era composers')
 
-print(page_py.exists("List of Classical-era composers"))
+print(f'Page - Title: {page_py.title}')
 
 #Ask user of music era of interest. Return list of composers of that era
 def main():
