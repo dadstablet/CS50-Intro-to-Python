bbe5b3688c9b7f3cbddacb29f884cfec3e182ac8
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-10 00:04:02 +0000

    Mon, Feb 9, 2026, 4:04 PM -08:00

--- project/project.py
+++ project/project.py
@@ -5,11 +5,15 @@ page_py = wiki_wiki.page('List of Classical-era composers')
 
 # print(f'Page - Title: {page_py.title}')
 # print(page_py.summary[0:60])
-def print_sections(sections, level=0):
-    for s in sections:
-        print(s)
 
-print_sections(page_py.sections)
+# def print_sections(sections, level=0):
+#     for s in sections:
+#         print(s)
+
+# print_sections(page_py.sections)
+
+secti
+
 
 #Ask user of music era of interest. Return list of composers of that era
 def main():
