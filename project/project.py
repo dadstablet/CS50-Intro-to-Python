ae36a39be98d787ce7558d10a08b5e7c1eed0b2b
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-12 23:02:53 +0000

    Thu, Feb 12, 2026, 3:02 PM -08:00

--- project/project.py
+++ project/project.py
@@ -1,7 +1,8 @@
 import wikipediaapi
 
-wiki_wiki = wikipediaapi.Wikipedia(user_agent='MusicProjectBot (nzakaria@hl.agency)', language='en')
+wiki_wiki = wikipediaapi.Wikipedia(user_agent='MusicProjectBot (nzakaria@hl.agency)', language='en', extract_format=wikipediaapi.ExtractFormat.HTML)
 page_py = wiki_wiki.page('List of Classical-era composers')
+print(page_py)
 
 # print(f'Page - Title: {page_py.title}')
 # print(page_py.summary[0:60])
@@ -14,15 +15,6 @@ page_py = wiki_wiki.page('List of Classical-era composers')
 
 # print_sections(page_py.sections)
 
-"""
-"""
-wiki_html = wikipediaapi.Wikipedia(
-    user_agent='MyProjectName (merlin@example.com)',
-    language='en',
-    extract_format=wikipediaapi.ExtractFormat.HTML
-)
-p_html = wiki_html.page("List of Classical-era composers")
-print(p_html.text)
 
 
 #Ask user of music era of interest. Return list of composers of that era
