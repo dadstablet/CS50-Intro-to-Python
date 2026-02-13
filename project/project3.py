564a16c45a2e36e4365a70bba2995e6fe846770f
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-13 18:11:05 +0000

    Fri, Feb 13, 2026, 10:11 AM -08:00

--- project/project3.py
+++ project/project3.py
@@ -11,5 +11,6 @@ text = BeautifulSoup(wiki_html, 'html.parser')
 #next steps: return eras, create list of composers based on eras
 #composers aren't in wikitable, rather a bulletted list under designated era
 
-eras = text.find_all('h2')
+eras = text.find_all('h2') #maybe add id=input of user
+# eras = text.find_all(class_='mw-heading mw-heading2')
 print(eras)
