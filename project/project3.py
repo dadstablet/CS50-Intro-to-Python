7c6a7b5950a2796c4756ba8e52fb146e7c04d0bc
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-13 18:18:44 +0000

    Fri, Feb 13, 2026, 10:18 AM -08:00

--- project/project3.py
+++ project/project3.py
@@ -5,12 +5,12 @@ wiki = wikipedia.WikipediaPage('List of Classical-era composers')
 wiki_html = wiki.html()
 
 text = BeautifulSoup(wiki_html, 'html.parser')
-# print(text.prettify())
+print(text.prettify())
 # print(text.get_text())
 
 #next steps: return eras, create list of composers based on eras
 #composers aren't in wikitable, rather a bulletted list under designated era
 
-eras = text.find_all('h2') #maybe add id=input of user
-# eras = text.find_all(class_='mw-heading mw-heading2')
-print(eras)
+# eras = text.find_all('h2') #maybe add id=input of user
+# # eras = text.find_all(class_='mw-heading mw-heading2')
+# print(eras)
