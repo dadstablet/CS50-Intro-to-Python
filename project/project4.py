bb2f3d57acdc598258d60e20793b82f59aa52a3b
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-13 18:43:04 +0000

    Fri, Feb 13, 2026, 10:43 AM -08:00

--- project/project4.py
+++ project/project4.py
@@ -13,7 +13,7 @@ text_list = text.splitlines()
 
 names = []
 for name in text_list:
-    if re.search(r'\(*\)', name):
+    if re.search(r' \(\d{4}–\d{4}\)', name):
         names.append(name)
 
 print(names)
