80d699eb7fc6d94969216c39ee52c3255d7be8ee
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-13 22:53:45 +0000

    Fri, Feb 13, 2026, 2:53 PM -08:00

--- project/project4.py
+++ project/project4.py
@@ -33,9 +33,7 @@ for composer in text_list:
 print(df)
 
 def categorize_era(x):
-    if 1400 <= x <= 1600:
-        return "Renaissance"
-    elif 1600 <= x <= 1760:
+    if 1600 <= x <= 1760:
         return "Baroque"
     elif 1730 <= x <= 1760:
         return "Classical"
@@ -45,15 +43,4 @@ def categorize_era(x):
         return "Modernist"
     elif 1930 <= x <= datetime.date.today():
         return "Postmodernist"
-
-#if life in era. return composer
-# eras = {
-#     Medieval : '500-1400',
-#     Renaissance : '1400-1600',
-#     Baroque : '1600-1760',
-#     Classical : '1730-1820',
-#     Romantic : '1815-1910',
-#     Modernist : '1890-1950',
-#     Postmodernist : '1930-2026'
-# }
 
