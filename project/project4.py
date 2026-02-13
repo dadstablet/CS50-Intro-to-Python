b95f9402bf175a3010f70469e30f2e4e84789741
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-13 22:17:45 +0000

    Fri, Feb 13, 2026, 2:17 PM -08:00

--- project/project4.py
+++ project/project4.py
@@ -29,10 +29,9 @@ for composer in text_list:
         # life_test.append(life.group(1))
         grouping = re.search(r'([\D]+) \((\d{4})–(\d{4})\)', composer) #need to remove \xa0[de] from name
         name, birth, death= grouping.groups()
-        df = name + birth + death + ""
-        # append({"name":name, "birth_year":birth, "death_year":death})
-# df = pd.DataFrame(data=d)
-print(grouping.groups())
+        df.loc[len(df)] = [name,birth,death,""]
+
+print(df)
 
 #if life in era. return composer
 # eras = {
