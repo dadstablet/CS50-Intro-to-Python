dfe5276ca6119620a24bd7427935c4207affb0cf
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-13 21:04:01 +0000

    Fri, Feb 13, 2026, 1:04 PM -08:00

--- project/project4.py
+++ project/project4.py
@@ -18,25 +18,25 @@ for composer in text_list:
     if re.search(r' \(\d{4}–\d{4}\)$', composer):
         # composers.update(composer.)
         # life = re.search(r'\((\d{4})–(\d{4})\)$', composer)
-        grouping = re.search(r'([\D]+) \((\d{4}–\d{4})\)', composer) #need to remove \xa0[de] from name
-        name, life = grouping.groups()
+        grouping = re.search(r'([\D]+) \((\d{4})–(\d{4})\)', composer) #need to remove \xa0[de] from name
+        name, birth, death= grouping.groups()
         # name.strip('\xa0[de]')
         # life = re.search(r'\((\d{4}–\d{4})\)', composer)
         # life = life.group(1)
         # name = re.search(r'([\D]+ )', composer)
         # name = name.group(1).strip()
         # life_test.append(life.group(1))
-        composers.update({name:life})
+        life_test.append(death)
 
-print(composers)
+print(life_test)
 #if life in era. return composer
-eras = {
-    Medieval : '500-1400',
-    Renaissance : '1400-1600',
-    Baroque : '1600-1760',
-    Classical : '1730-1820',
-    Romantic : '1815-1910',
-    Modernist : '1890-1950',
-    Postmodernist : '1930-2026'
-}
+# eras = {
+#     Medieval : '500-1400',
+#     Renaissance : '1400-1600',
+#     Baroque : '1600-1760',
+#     Classical : '1730-1820',
+#     Romantic : '1815-1910',
+#     Modernist : '1890-1950',
+#     Postmodernist : '1930-2026'
+# }
 
