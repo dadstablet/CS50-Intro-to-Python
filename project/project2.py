b2149cc288cadbf88a27ecdb7e87d06743f3eba8
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-13 17:12:49 +0000

    Fri, Feb 13, 2026, 9:12 AM -08:00

--- project/project2.py
+++ project/project2.py
@@ -2,12 +2,6 @@ import wikipedia
 
 wiki = wikipedia.WikipediaPage('List of Classical-era composers')
 
-# Extract the plain text content of the page, excluding images, tables, and other data.
-text = wiki.content
+categories = wiki.categories
 
-# Replace '==' with '' (an empty string)
-text = text.replace('==', '')
-
-# Replace '\n' (a new line) with '' & end the string at $1000.
-text = text.replace('\n', '')[:-12]
-print(text)
+print(categories)
