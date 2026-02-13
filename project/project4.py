0a6dfb75b56b21a1f4dd0d999f4dad4bcf3e9e52
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-13 19:35:22 +0000

    Fri, Feb 13, 2026, 11:35 AM -08:00

--- project/project4.py
+++ project/project4.py
@@ -20,7 +20,7 @@ for composer in text_list:
         life = re.search(r'\((\d{4}–\d{4})\)', composer)
         life = life.group(1)
         name = re.search(r'([A-Za-z ]+)', composer)
-        name = name.group(1)
+        name = name.group(1).strip()
         # life_test.append(life.group(1))
         composers.update({name:life})
 
