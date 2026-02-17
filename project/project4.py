34a9d8e8b0f22ac3604ecb413f6b2bd6a684bccd
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-17 21:49:37 +0000

    Tue, Feb 17, 2026, 1:49 PM -08:00

--- project/project4.py
+++ project/project4.py
@@ -20,8 +20,8 @@ for composer in text_list:
     if re.search(r' \(\d{4}–\d{4}\)$', composer):
         grouping = re.search(r'([\D]+) \((\d{4})–(\d{4})\)', composer) #need to remove \xa0[de] from name
         name, birth, death= grouping.groups()
-        composers.loc[len(composers)] = [name,birth,death,""]
-# print(composers)
+        composers.loc[len(composers)] = [name,birth,death,""] #need to convert birth and death into int. right now strs
+
 
 # def categorize_era(x):
 #     if 1600 <= int(x) <= 1760:
@@ -31,21 +31,21 @@ for composer in text_list:
 #     elif 1815 <= int(x) <= 1910:
 #         return "Romantic"
 
-def categorize_era(x):
-    if 1600 <= x['birth_year'] <= 1760:
-        return "Baroque"
-    elif 1730 <= x['birth_year'] <= 1760:
-        return "Classical"
-    elif 1815 <= x['birth_year'] <= 1910:
-        return "Romantic"
+# def categorize_era(x):
+#     if 1600 <= x['birth_year'] <= 1760:
+#         return "Baroque"
+#     elif 1730 <= x['birth_year'] <= 1760:
+#         return "Classical"
+#     elif 1815 <= x['birth_year'] <= 1910:
+#         return "Romantic"
 
 #now categorize composer period given birth and death dates. add era as a column of type list to table
 #look through all birth dates. append era to era list (empty list)
 #look through all death dates. append era to era list if era does not exist already in list
 
-era_list = []
-composers['period'] = composers.apply(categorize_era, axis=1)
-print(composers)
+# era_list = []
+# composers['period'] = composers.apply(categorize_era, axis=1)
+# print(composers)
 
 #prompt user for period. return random composer in period. if lived in overlap, have chance to be in either period
 
