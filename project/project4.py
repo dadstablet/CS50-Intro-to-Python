052a68f42df0be98915014c1af436553ae58c90f
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-17 22:21:18 +0000

    Tue, Feb 17, 2026, 2:21 PM -08:00

--- project/project4.py
+++ project/project4.py
@@ -21,7 +21,7 @@ for composer in text_list:
         grouping = re.search(r'([\D]+) \((\d{4})–(\d{4})\)', composer) #need to remove \xa0[de] from name
         name, birth, death= grouping.groups()
         composers.loc[len(composers)] = [name,birth,death,""] #need to convert birth and death into int. right now strs
-composers['birth_year'].astype(int)
+composers['birth_year'] = pd.to_numeric(composers['birth_year'])
 composers['death_year'].astype(int)
 
 datatypes = composers.dtypes
