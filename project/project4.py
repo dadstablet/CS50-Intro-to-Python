db7e07bda0e82ace07a4777409306db7d35f9b61
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-18 00:44:53 +0000

    Tue, Feb 17, 2026, 4:44 PM -08:00

--- project/project4.py
+++ project/project4.py
@@ -45,8 +45,7 @@ def categorize_era(x):
 #look through all death dates. append era to era list if era does not exist already in list
 
 era_list = []
-for r in composers:
-    era_list.append(categorize_era(composers))
+ara_list = composers['birth_year'].tolist()
 # composers['period'] = composers.apply(categorize_era, axis=1)
 # print(composers)
 print(era_list)
