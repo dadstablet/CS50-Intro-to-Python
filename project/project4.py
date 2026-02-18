985714c2a92ec8c060e35ca405e80934ce432390
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-18 18:35:03 +0000

    Wed, Feb 18, 2026, 10:35 AM -08:00

--- project/project4.py
+++ project/project4.py
@@ -32,7 +32,7 @@ composers['death_year'] = pd.to_numeric(composers['death_year'], downcast='integ
 #     elif 1815 <= int(x) <= 1910:
 #         return "Romantic"
 
-def categorize_era(x, y):
+def categorize_era(x):
     era_tuple = tuple()
     if 1600 <= x <= 1760:
         era_tuple += 'Baroque'
@@ -45,11 +45,11 @@ def categorize_era(x, y):
 #look through all birth dates. append era to era list (empty list)
 #look through all death dates. append era to era list if era does not exist already in list
 
-era_list = []
-era_list = categorize_era(composers)
-# composers['period'] = composers.apply(categorize_era, axis=1)
+# era_list = []
+# era_list = categorize_era(composers)
+composers['period'] = composers.apply(categorize_era, axis=1)
 # print(composers)
-print(era_list)
+print(composers)
 
 #prompt user for period. return random composer in period. if lived in overlap, have chance to be in either period
 
