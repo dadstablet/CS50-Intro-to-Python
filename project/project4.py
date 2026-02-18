1f2f23355aba7b776a7ad91cb7be063244726dd6
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-18 18:36:14 +0000

    Wed, Feb 18, 2026, 10:36 AM -08:00

--- project/project4.py
+++ project/project4.py
@@ -47,7 +47,7 @@ def categorize_era(x):
 
 # era_list = []
 # era_list = categorize_era(composers)
-composers['period'] = composers.apply(categorize_era, axis=1)
+composers['period'] = composers.apply(categorize_era['birth_year'])
 # print(composers)
 print(composers)
 
