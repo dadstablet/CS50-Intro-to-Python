4c4b9c52586345f324d1c6355941eae2298d6cab
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-18 22:48:31 +0000

    Wed, Feb 18, 2026, 2:48 PM -08:00

--- project/project4.py
+++ project/project4.py
@@ -51,7 +51,7 @@ for index, row in composers.iterrows():
 #prompt user for period. return random composer in period. if lived in overlap, have chance to be in either period
 
 select_period = input("Baroque, Classical, or Romantic? ")
-composers[composers['period'].apply(lambda x: select_period in x)]
+print(composers[composers['period'].apply(lambda x: select_period in x)])
 
 # def return_composer():
 
