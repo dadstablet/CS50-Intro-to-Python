84c3e8d07efab781930c5e1d2ecddf97e98237b8
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-19 23:40:24 +0000

    Thu, Feb 19, 2026, 3:40 PM -08:00

--- project/test_project.py
+++ project/test_project.py
@@ -2,17 +2,16 @@ from project import get_composer_table, categorize_era, select_period
 import pytest
 
 def test_get_composer_table():
-    get_composer_table() = composers
-    assert len(get_composer_table() ) == 669
-    assert composers['name'].dtype == str
+    assert len(get_composer_table()) == 669
 
 def test_categorize_era():
     assert categorize_era(1600) == 'Baroque'
-    assert cateogorize_era(1735) == 'Baroque'
+    assert categorize_era(1735) == 'Baroque'
     assert categorize_era(1815) == 'Classical'
 
 def test_select_period():
+    with pytest.raises(ValueError):
+        select_period('hello world')
     assert select_period('Bach') == 'Baroque'
     assert select_period('Romantic') == 'Romantic'
-    assert select_period('hello world') == ValueError
     assert select_period('clAssIcal') == 'Classical'
