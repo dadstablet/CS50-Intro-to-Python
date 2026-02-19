cf9a6df6f20f40ac4c57e9ddd812cb602cae9f87
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-19 21:22:37 +0000

    Thu, Feb 19, 2026, 1:22 PM -08:00

--- project/project4.py
+++ project/project4.py
@@ -45,16 +45,15 @@ def categorize_era(x):
 def main():
     """prompt user for period. return random composer in period. if lived in overlap, have chance to be in either period"""
     composers = get_composer_table()
-    select_period(composers)
 
-def select_period(composers: pd.DataFrame):
-    user_input = input('Baroque, Classical, or Romantic? ').capitalize()
+    #use a for loop that breaks once correct input is provided
     try:
-        selected_composers = composers[composers['period'].apply(lambda x: user_input in x)]
+        select_period = input('Baroque, Classical, or Romantic? ')
+        selected_composers = composers[composers['period'].apply(lambda x: select_period in x)]
         rand_comps = selected_composers['name'].sample(3).values
         print(f'Try out {rand_comps[0]}, {rand_comps[1]}, or {rand_comps[2]}')
     except ValueError:
-        select_period()
+        select_period
 
 if __name__ == '__main__':
     main()
