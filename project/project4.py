66bde9cb2cdb01c67b0584e83581bf75995b97bf
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-19 21:07:02 +0000

    Thu, Feb 19, 2026, 1:07 PM -08:00

--- project/project4.py
+++ project/project4.py
@@ -14,16 +14,15 @@ def get_composer_table():
 
     composers = pd.DataFrame(columns=['name', 'birth_year', 'death_year', 'period'])
     for composer in text_list:
-        if re.search(r' \(\d{4}–\d{4}\)$', composer):
-            grouping = re.search(r'([\D]+) \((\d{4})–(\d{4})\)', composer) #need to remove \xa0[de] from name
+        if re.search(r' \(\d{4}–\d{4}\)$', composer): #lines that end in (YYYY-YYYY) are composers
+            grouping = re.search(r'([\D]+) \((\d{4})–(\d{4})\)', composer) #capture name, birth_year, and death_year. unable to figure out how to remove \xa0[de] from name
             name, birth, death= grouping.groups()
-            composers.loc[len(composers)] = [name,birth,death,""] #need to convert birth and death into int. right now strs
+            composers.loc[len(composers)] = [name,birth,death,""] #need to convert birth and death into column of ints. right now strs
     composers['birth_year'] = pd.to_numeric(composers['birth_year'], downcast='integer', errors='coerce')
     composers['death_year'] = pd.to_numeric(composers['death_year'], downcast='integer', errors='coerce')
     composers['period'] = [[] for _ in range(len(composers))]
 
-
-    # now categorize composer period(s) given birth and death dates.
+    # now categorize composer period(s) given birth and death dates. fill in empty list column of period
     for index, row in composers.iterrows():
         row['period'].append(categorize_era(row['birth_year']))
         if categorize_era(row['death_year']) not in row['period']:
@@ -32,22 +31,21 @@ def get_composer_table():
             pass
 
     return composers
-    # print(composers)
 
-#create function to 'classify' the period(s) of composer
 def categorize_era(x):
+    """classify the period(s) of composer"""
     if 1600 <= int(x) <= 1760:
-        return "Baroque"
+        return 'Baroque'
     elif 1730 <= int(x) <= 1820:
-        return "Classical"
+        return 'Classical'
     elif 1815 <= int(x) <= 1910:
-        return "Romantic"
+        return 'Romantic'
 
 
-#prompt user for period. return random composer in period. if lived in overlap, have chance to be in either period
 def main():
+    """prompt user for period. return random composer in period. if lived in overlap, have chance to be in either period"""
     composers = get_composer_table()
-    select_period = input("Baroque, Classical, or Romantic? ")
+    select_period = input('Baroque, Classical, or Romantic? ')
     selected_composers = composers[composers['period'].apply(lambda x: select_period in x)]
     rand_comps = selected_composers['name'].sample(3).values
     print(f'Try out {rand_comps[0]}, {rand_comps[1]}, or {rand_comps[2]}')
