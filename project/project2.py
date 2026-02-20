7abe10b863a2446e84d1ce4f2f2c28fa53e2c1cb
Noah Zakaria <153245272+dadstablet@users.noreply.github.com>
2026-02-20 23:51:57 +0000

    Fri, Feb 20, 2026, 3:51 PM -08:00

--- project/project2.py
+++
@@ -1,7 +0,0 @@
-import wikipedia
-
-wiki = wikipedia.WikipediaPage('List of Classical-era composers')
-
-# print(wiki.section("Early Galante era composers – Transition from Baroque to Classical (born before 1710)"))
-
-print(wiki.content)
